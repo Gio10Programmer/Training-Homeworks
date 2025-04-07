78259000f03ed9106300ecdbac46dd498c6c054f
Gio10Programmer <gio991520@gmail.com>
2025-04-07 21:30:17 +0400

    commit

---
+++ Level 4/Homework/python lesson 4.py
@@ -0,0 +1,64 @@
+#დავალება 1
+#bug არის დაშვებული შეცდომა და ამ ხარვეზის შედეგად ვიღებთ პროგრამის გაუმართაობას. 
+#debugging არის-bug-ების პოვნა და პრობლემის გამოსწორება კოდში. ანუ,როცა პროგრამა არასწორად მუშაობს(შეიცავს bug-ს), ჩვენ ვიწყებთ debugging-ს და ვეცდებით გავიგოთ, სად და რატომ ხდება შეცდომა,და მერე ვასწორებთ.
+ 
+#დავალება 2
+#არასწორია
+#num=1
+#num=2
+
+#print num+num
+#სწორია
+#num1=1
+#num2=2
+
+#print(num1+num2)
+
+#დავალება 3
+num1=50
+num2=50
+
+print(num1+num2)
+print(num1-num2)
+print(num1*num2)
+print(num1/num2)
+
+num1=100
+num2=100
+
+print(num1+num2)
+print(num1-num2)
+print(num1*num2)
+print(num1/num2)
+
+num1=150
+num2=150
+
+print(num1+num2)
+print(num1-num2)
+print(num1*num2)
+print(num1/num2)
+
+num1=1000
+num2=1500
+
+print(num1+num2)
+print(num1-num2)
+print(num1*num2)
+print(num1/num2)
+
+num1=500
+num2=1000
+
+print(num1+num2)
+print(num1-num2)
+print(num1*num2)
+print(num1/num2)
+
+age1=16
+age2=17
+
+print(age1+age2)
+
+#დროებითი მეხსიერება (temporary memory) იგივე RAM (Random Acces Memory)- არის კომპიუტერის ან ტელფონის მეხსიერება, რომელიც გამოიყენება დროებით მონაცემების შესანახად მაშინ,როცა პროგრამები ან სისტემები მუშაობენ.
+#დროებითი მეხსიერება გვეხმარება სწრაფად და ეფექტურად  დრობითი ამოცანების შესრულებაში.
